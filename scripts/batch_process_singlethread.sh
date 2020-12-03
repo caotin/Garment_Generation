ef4483be317d818780482fb7e599e610a84fbf3d
Cao Tín <caotin15cntt@gmail.com>
2020-12-03 21:59:24 +0700

    Update batch_process_singlethread.sh

--- scripts/batch_process_singlethread.sh
+++ scripts/batch_process_singlethread.sh
@@ -6,8 +6,11 @@ do
 		for u in $(seq 0 7)
 		do
    			echo "c $c  m $m  u $u"
+			u8=`expr 8 \* $u`
+			id0=`expr 0 + $u8`
 			#../build/ProcessMesh_compare 2 $c $m $u
-			python prepare_data.py $1 $2 $c $m $u
+			python prepare_data.py $1 $2 $c $m $u $id0
+			wait
 		done
 	done
 done
